abb464f23dde7d2500d32d313960e27a167e8dff
Danijel Pavić <danijelpavic21@gmail.com>
2017-03-15 21:19:33 +0100

    Napisali smo TestPolja.

---
+++ PotapanjeBrodova/PotapanjeBrodova/Polje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotapanjeBrodova
+{
+    public class Polje : IEquatable<Polje>
+    {
+        public readonly int Redak;
+        public readonly int Stupac;
+
+        public Polje(int redak, int stupac)
+        {
+            Redak = redak;
+            Stupac = stupac;
+        }
+
+        public bool Equals(Polje other)
+        {
+            return Redak == other.Redak && Stupac == other.Stupac;
+        }
+    }
+}
---
+++ PotapanjeBrodova/Test/TestPolja.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
+namespace Test
+{
+    [TestClass]
+    public class TestPolja
+    {
+        [TestMethod]
+        public void Polje_RedakIStupacJednakiSuArgumentimaKontruktora()
+        {
+            Polje p = new Polje(2, 3);
+            Assert.AreEqual(2, p.Redak);
+            Assert.AreEqual(3, p.Stupac);
+
+        }
+
+        [TestMethod]
+        public void Polje_PoljaZaDvaPoljaKojaImajuIsteKoordinateMetodaEqualsVraćaTrue()
+        {
+            Polje p1 = new Polje(2, 3);
+            Polje p2 = new Polje(2, 3);
+            Assert.IsTrue(p1.Equals(p2));
+        }
+
+        [TestMethod]
+        public void Polje_PoljaZaDvaPoljaKojaNemajuIsteKoordinateMetodaEqualsVraćaFalse()
+        {
+            Polje p1 = new Polje(2, 3);
+            Polje p2 = new Polje(2, 4);
+            Assert.IsFalse(p1.Equals(p2));
+            Polje p3 = new Polje(1, 3);
+            Assert.IsFalse(p1.Equals(p3));
+        }
+
+    }
+}
