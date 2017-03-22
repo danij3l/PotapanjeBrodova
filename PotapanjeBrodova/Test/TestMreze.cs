050d5c5dc1f485af03810cb4b916dc59888b2d9b
Danijel Pavić <danijelpavic21@gmail.com>
2017-03-22 20:38:42 +0100

    Riješili smo implementaciju mreže i testove.

---
+++ PotapanjeBrodova/Test/TestMreze.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using PotapanjeBrodova;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class TestMreze
+    {
+        [TestMethod]
+        public void Mreza_DajSlobodnaPoljaVracaSvaPoljaZaPocetnuMrezu()
+        {
+            Mreza m = new Mreza(5, 5);
+            Assert.AreEqual(25, m.DajSlobodnaPolja().Count());
+        }
+
+        [TestMethod]
+        public void Mreza_DajSlobodnaPoljaVraca24PoljaZaMrezu5x5NakonJednogUklonjenogPoljaZadanogRetkomIStupcem()
+        {
+            Mreza m = new Mreza(5, 5);
+            m.UkloniPolje(1, 1);
+            Assert.AreEqual(24, m.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(1, 1)));
+        }
+
+        [TestMethod]
+        public void Mreza_DajSlobodnaPoljaVraca24PoljaZaMrezu5x5NakonJednogUklonjenogPolja()
+        {
+            Mreza m = new Mreza(5, 5);
+            m.UkloniPolje(new Polje(1, 1));
+            Assert.AreEqual(24, m.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(1, 1)));
+        }
+
+        [TestMethod]
+        public void Mreza_DajSlobodnaPoljaVraca23PoljaZaMrezu5x5NakonDvaUklonjenogPolja()
+        {
+            Mreza m = new Mreza(5, 5);
+            m.UkloniPolje(1, 1);
+            m.UkloniPolje(4, 4);
+            Assert.AreEqual(23, m.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(1, 1)));
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(4, 4)));
+        }
+
+        [TestMethod]
+        public void Mreza_UkloniPoljeBacaIznimkuZaNepostojeciRedak()
+        {
+            try
+            {
+                Mreza m = new Mreza(5, 5);
+                m.UkloniPolje(6, 1);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+
+        }
+    }
+}
