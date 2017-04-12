e9a09cde38ebb9f8fcd10921c99da7a04a345cfb
Danijel Pavić <danijelpavic21@gmail.com>
2017-04-12 19:21:17 +0200

    Implementiran terminator polja i dodani testovi za njega.

--- PotapanjeBrodova/PotapanjeBrodova/TerminatorPolja.cs
+++ PotapanjeBrodova/PotapanjeBrodova/TerminatorPolja.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PotapanjeBrodova
 {
@@ -13,10 +12,16 @@ namespace PotapanjeBrodova
             this.mreža = mreža;
         }
 
-        public void UkloniPolja()
+        public void UkloniPolja(IEnumerable<Polje> polja)
         {
-            // todo
-            // mreza.Ukloni polja
+            IEnumerable<Polje> sortirana = polja.Sortiraj();
+            int r0 = Math.Max(sortirana.First().Redak - 1, 0);
+            int s0 = Math.Max(sortirana.First().Stupac - 1, 0);
+            int r1 = Math.Min(sortirana.Last().Redak + 2, mreža.Redaka);
+            int s1 = Math.Min(sortirana.Last().Stupac + 2, mreža.Stupaca);
+            for (int r = r0; r < r1; ++r)
+                for (int s = s0; s < s1; ++s)
+                    mreža.UkloniPolje(r, s);
         }
 
         private Mreza mreža;
