3d571ba8c50a095a6981f83d438ed16c96f5d060
Danijel Pavić <danijelpavic21@gmail.com>
2017-05-10 22:57:57 +0200

    Slučajni Pucač:: Traženje najboljeg polja za gađanje.

--- PotapanjeBrodova/PotapanjeBrodova/SlučajniPucač.cs
+++ PotapanjeBrodova/PotapanjeBrodova/SlučajniPucač.cs
@@ -7,23 +7,57 @@ namespace PotapanjeBrodova
 {
     public class SlučajniPucač : IPucač
     {
-        public SlučajniPucač(Mreža mreža, int duljinaBroda)
+        public SlučajniPucač(Mreza mreza, List<int> brodovi)
         {
-            this.mreža = mreža;
-            this.duljinaBroda = duljinaBroda;
-
+            this.mreza = mreza;
+            najdužiBrod = brodovi.Max();
         }
+
+        Polje TraziNajboljePolje()
+        {
+            foreach (IEnumerable<IEnumerable<Polje>> nizPolja in mreza.DajNizoveSlobodnihPolja(najdužiBrod))
+            {
+                foreach (Polje polje in nizPolja)
+                {
+                    if (!mapa.ContainsKey(polje))
+                    {
+                        mapa.Add(polje, 0);
+                    }
+                    else
+                    {
+                        mapa[polje] += 1;
+                    }
+                }
+            }
+            var poslozeniRijecnik = mapa.OrderBy(x => x.Value);
+
+            int max = poslozeniRijecnik.First().Value;
+            foreach (KeyValuePair<Polje, int> izborPolja in poslozeniRijecnik)
+            {
+                if (izborPolja.Value != max)
+                    mapa.Remove(izborPolja.Key);
+            }
+            List<Polje> keyList = new List<Polje>(mapa.Keys);
+            return keyList[slucajniBroj.Next(keyList.Count())];
+        }
+
         public Polje Gađaj()
         {
+
+            // random gadjanje, mora dobiti mrezu od topnistva, od mreze trazi najdulji brod (konstruktor)
+            // i trazi polja gdje moze staviti taj najdulji brod. Tezina polja +
             throw new NotImplementedException();
         }
 
-        public void ObradiGađanje(RezultatGađanja rezultat)
+        public void ObradiGađanja(RezultatGađanja rezultat)
         {
+            // ako je brod potopljen, eliminira polja oko broda
             throw new NotImplementedException();
         }
 
-        private Mreza mreža;
-        private int duljinaBroda;
+        private Dictionary<Polje, int> mapa;
+        private Mreza mreza;
+        private int najdužiBrod;
+        private Random slucajniBroj = new Random();
     }
 }
